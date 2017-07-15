d9c061a68ec63007cac2c78cbc58030f535612e2
Perajit Pasupat <perajit.pasupat@thomsonreuters.com>
2017-07-15 18:42:53 +0700

    Set up react

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  devtool: 'eval',
+  entry: './src/ui/index.js',
+  output: {
+    publicPath: '/static/',
+    path: path.join(__dirname, 'static'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+}
