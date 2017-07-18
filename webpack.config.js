0d15f450e9360fb3ceb8e71ba45ac20798d1e369
Perajit Pasupat <perajit.pasupat@thomsonreuters.com>
2017-07-18 23:51:32 +0700

    Modify config

--- webpack.config.js
+++ webpack.config.js
@@ -35,5 +35,13 @@ module.exports = {
         ]
       }
     ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      '/api/*': {
+        target: 'http://localhost:8081/'
+      }
+    }
   }
 }
